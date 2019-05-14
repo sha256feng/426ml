ca6b365dcbb173123bc7d3bf53568571e6a1657b
ljc14 <ljc14@software.nju.edu.cn>
2019-05-14 04:52:34 -0400

    final

---
+++ test2.py
@@ -0,0 +1,13 @@
+from numpy import array, dot
+from qpsolvers import solve_qp
+
+M = array([[1., 2., 0.], [-8., 3., 2.], [0., 1., 1.]])
+P = dot(M.T, M)  # quick way to build a symmetric matrix
+q = dot(array([3., 2., 3.]), M).reshape((3,))
+G = array([[1., 2., 1.], [2., 0., 1.], [-1., 2., -1.]])
+h = array([3., 2., -2.]).reshape((3,))
+A = array([1., 1., 1.])
+b = array([1.])
+
+print(P.shape, q.shape, G.shape, h.shape, A.shape, b.shape)
+print ("QP solution:", solve_qp(P, q, G, h, A, b))
